7831692ac42bd9a37bf1244f217a6e0c120e2e06
yuichivls <viniciuslimadasilva4@gmail.comr>
2020-03-12 20:51:58 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cadPessoaContador';
+  nome = null;
+  pessoas = [];
+  contador = 0;
+  adicionar(nome){
+    this.pessoas.push(this.nome);
+    this.nome = null;
+    this.contador = this.contador + 1;
+  }
+  deletar(nome){
+    this.pessoas.splice(this.nome, 1);
+    this.contador = this.contador - 1;
+  }
 }
